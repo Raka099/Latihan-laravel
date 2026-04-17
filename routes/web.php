7b304e77951ba59769767ce078bde3f7ee6c7fbf
Raka099 <rakamulfaidzy5@gmail.com>
2026-04-17 15:30:50 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/user/{id}', function ($id) {
+    return "profile user dengan ID :".$id;
+});
+
+Route::get('/produk/{slug?}', function ($slug="semua-produk") {
+    return "menampilkan :".$slug;
+});
+
+Route::get('/Mahasiswa',[MahasiswaController::class,'index'] );
